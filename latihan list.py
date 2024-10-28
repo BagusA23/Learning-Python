a1ac4ee9b908dff04ed3ec3904a59b43c01ef395
bagusanardiansyah@gmail.com <bagusA23>
2024-10-28 21:11:10 +0700

    update latihan list

--- latihan list.py
+++ latihan list.py
@@ -1,21 +1,46 @@
 
-list_buku = []
+# list_buku = []
 
-while True:
-    print("\nmasukan data buku ")
-    judul = input("masukan judul buku \t: ")
-    penulis =  input("masukan penulis buku \t: ")
+# while True:
+#     print("\nmasukan data buku ")
+#     judul = input("masukan judul buku \t: ")
+#     penulis =  input("masukan penulis buku \t: ")
+    
+#     buku_baru = [judul,penulis]
+#     list_buku.append(buku_baru)
+    
+#     print("\n\n","="*20)
+#     for index, buku in enumerate(list_buku):
+#         print(f"{index+1}\t|{buku[0]}\t|{buku[1]}")
+        
+#     lanjut = input("apakah anda ingin lanjut {y/n} : ")
+    
+#     if lanjut == "n":
+#         break
+
+# print("PROGRAM SELESAI")
+def tambah_buku(list_buku):
+    print("\nMasukan data buku ")
+    judul = input("Masukan judul buku \t: ")
+    penulis =  input("Masukan penulis buku \t: ")
     
     buku_baru = [judul,penulis]
     list_buku.append(buku_baru)
-    
+
+def tampilkan_buku(list_buku):
     print("\n\n","="*20)
     for index, buku in enumerate(list_buku):
         print(f"{index+1}\t|{buku[0]}\t|{buku[1]}")
-        
-    lanjut = input("apakah anda ingin lanjut {y/n} : ")
-    
-    if lanjut == "n":
-        break
 
-print("PROGRAM SELESAI")
+def main():
+    list_buku = []
+    while True:
+        tambah_buku(list_buku)
+        tampilkan_buku(list_buku)
+        lanjut = input("Apakah anda ingin lanjut {y/n} : ")
+        if lanjut.lower() == "n":
+            break
+    print("PROGRAM SELESAI")
+
+if __name__ == "__main__":
+    main()
